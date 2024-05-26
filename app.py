4838419a0874168c094498229890f9cf1b673075
Quan <quancsf@gmail.com>
2024-05-26 23:22:40 +0700

    add app

---
+++ app.py
@@ -0,0 +1,61 @@
+import utils
+from flask import Flask, render_template, request
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain.llms import CTransformers
+
+app = Flask(__name__)
+
+extracted_data = utils.load_pdf("data/")
+text_chunks = utils.text_split(extracted_data)
+embeddings = utils.download_hugging_face_embeddings()
+faiss_index = utils.create_faiss_index(text_chunks, embeddings)
+
+prompt_template = """
+Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else.
+Helpful answer:
+"""
+
+PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+chain_type_kwargs = {"prompt": PROMPT}
+
+llm = CTransformers(
+    model="model/llama-2-7b-chat.ggmlv3.q4_0.bin",
+    model_type="llama",
+    config={'max_new_tokens': 512, 'temperature': 0.8}
+)
+
+qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=faiss_index.as_retriever(search_kwargs={'k': 2}),
+    return_source_documents=True,
+    chain_type_kwargs=chain_type_kwargs
+)
+
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    result=qa({"query": input})
+    print("Response : ", result["result"])
+    return str(result["result"])
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port= 5000, debug= True)
