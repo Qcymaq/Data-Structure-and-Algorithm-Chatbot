bb8c6117fdedadfe80916663fe8cb9f14f9a9ff0
Quan <quancsf@gmail.com>
2024-05-27 12:42:10 +0700

    add app

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "q
 chain_type_kwargs = {"prompt": PROMPT}
 
 llm = CTransformers(
-    model="model/llama-2-7b-chat.ggmlv3.q4_0.bin",
+    model="model/llama-2-7b-chat.ggmlv3.q5_1.bin",
     model_type="llama",
     config={'max_new_tokens': 512, 'temperature': 0.8}
 )
